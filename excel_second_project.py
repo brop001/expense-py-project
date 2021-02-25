51a0461ae2181049e96b7f1441bb233b1c173b3b
brop001 <bropmail@gmail.com>
2021-02-26 00:00:51 +0100

    able to generate files by month and year able to add labels able to add charts

--- excel_second_project.py
+++ excel_second_project.py
@@ -2,10 +2,20 @@ import openpyxl
 from openpyxl import utils
 import re
 import numbers
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-wb = openpyxl.load_workbook(filename='test.xlsx')
+output_folder_path = "output\\"
+file_name = "Combined_output.xlsx"
+
+file_path = output_folder_path + file_name
+config_path = "config.xlsx"
+
+wb = openpyxl.load_workbook(filename=file_path)
 ws = wb.active
-column_details = ws['C']
+column_details = ws['D']
 
 config_wb = openpyxl.load_workbook(filename='config.xlsx')
 config_ws = config_wb.active
@@ -35,47 +45,157 @@ def get_cell_value(row, column, worksheet):
         return int(value)
 
 
-
-def get_regex_str(i, cell3):
-    regex_mode = get_cell_value(row=i, column=2, worksheet=config_ws)
-    if regex_mode == "True":
-        regex_string = ".* " + str(cell3.value) + " .*"
-    elif regex_mode == "False":
-        regex_string = ".*" + str(cell3.value) + ".*"
+def get_regex_str(idx, dataframe):
+    regex_str = dataframe.at[idx, "Expense regex"]
+    regex_mode = dataframe.at[idx, "Search for the word"]
+    if regex_mode == True:
+        regex_string = ".* " + regex_str + " .*"
+    elif regex_mode == False:
+        regex_string = ".*" + regex_str + ".*"
     else:
         raise Exception("Can not detect regex mode!")
     print("regex string: " + regex_string)
     return regex_string
 
 
-# Expense regex;Search for the word;Expense description;Expense category;Other category;Expense nature
-column_regex = get_column("Expense regex", config_ws)
+def get_value_list(file_path, column):
+    df = pandas.read_excel(file_path, index_col=0)
+
+    value_list = []
+
+    for idx, row in df.iterrows():
+        value = df.at[idx, column]
+        if not value_list.count(value):
+            value_list.append(value)
+    if value_list.count(np.nan):
+        value_list.remove(np.nan)
+    print(value_list)
+    return value_list
 
 
-print_column(column_details)
+def get_and_write_category_amount(exp_category, exp_category_amount, file_path, dataframe_results, dataframe):
+    exp_cat_list = get_value_list(file_path, exp_category)
 
-
-for idx1, cell1 in enumerate(column_regex, start=1):
-    if(idx1 > 1):
-
+    for idx1, value in enumerate(exp_cat_list):
         expense_in_Ft = 0
-        regex_string = get_regex_str(idx1, cell1)
 
-        expense_description = get_cell_value(row=idx1, column=3, worksheet=config_ws)
-        expense_category = get_cell_value(row=idx1, column=4, worksheet=config_ws)
-        expense_other_category = get_cell_value(row=idx1, column=5, worksheet=config_ws)
-        expense_nature = get_cell_value(row=idx1, column=6, worksheet=config_ws)
+        for idx2, row in dataframe.iterrows():
+            if dataframe.at[idx2, exp_category] == value:
+                expense_in_Ft += dataframe.at[idx2, "Transaction amount"]
 
-        for idx2, cell2 in enumerate(column_details, start=1):
-            if re.search(regex_string, str(cell2.value)):
-                print("Regex found" + regex_string)
-                ws.cell(row=idx2, column=5, value=expense_description)
-                ws.cell(row=idx2, column=6, value=expense_category)
-                ws.cell(row=idx2, column=7, value=expense_other_category)
-                ws.cell(row=idx2, column=8, value=expense_nature)
-                expense_in_Ft += get_cell_value(row=idx2, column=2, worksheet=ws)
+        dataframe_results.at[idx1, exp_category] = value
+        dataframe_results.at[idx1, exp_category_amount] = expense_in_Ft
+        #dataframe_results = dataframe_results.append({exp_category: value, exp_category_amount: expense_in_Ft}, ignore_index=True)
 
-        ws.cell(row=idx1, column=9, value=regex_string)
-        ws.cell(row=idx1, column=10, value=expense_in_Ft)
+    values = dataframe_results[exp_category_amount].abs().dropna()
+    labels = dataframe_results[exp_category].dropna()
+    print(values)
+    print(labels)
 
-wb.save('test.xlsx')
+    plt.pie(values, labels=labels)
+    #plt.show()
+
+    return dataframe_results
+
+
+def write_chart(file_path, chart_list):
+
+    writer = pandas.ExcelWriter(file_path, engine='xlsxwriter')
+    df_results.to_excel(writer, sheet_name='Sheet1')
+
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+
+    for chart in range(len(chart_list)):
+        cat = df_results[chart_list[chart][0]]
+        val = df_results[chart_list[chart][1]]
+
+        cat_first_col = df_results.columns.get_loc(chart_list[chart][0])+1
+        cat_first_row = cat.index.get_loc(cat.first_valid_index())+1
+        cat_last_col = df_results.columns.get_loc(chart_list[chart][0])+1
+        cat_last_row = cat.index.get_loc(cat.last_valid_index())+1
+
+        val_first_col = df_results.columns.get_loc(chart_list[chart][1])+1
+        val_first_row = val.index.get_loc(val.first_valid_index())+1
+        val_last_col = df_results.columns.get_loc(chart_list[chart][1])+1
+        val_last_row = val.index.get_loc(val.last_valid_index())+1
+
+        pie_chart = (workbook.add_chart({'type': chart_list[chart][3]}))
+
+        pie_chart.add_series({
+            'name': chart_list[chart][0],
+            'categories': ['Sheet1', cat_first_row, cat_first_col, cat_last_row, cat_last_col],
+            'values': ['Sheet1', val_first_row, val_first_col, val_last_row, val_last_col],
+            'data_labels': {'value': True, 'percentage': True},
+        })
+
+        # Add a title.
+        pie_chart.set_title({'name': chart_list[chart][0]})
+
+        # Insert the chart into the worksheet.
+        worksheet.insert_chart(chart_list[chart][2], pie_chart, {'x_scale': 3, 'y_scale': 1})
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.close()
+
+
+# Expense regex;Search for the word;Expense description;Expense category;Other category;Expense nature
+
+df = pandas.read_excel(file_path, index_col=0)
+df_config = pandas.read_excel(config_path, index_col=0)
+print(df)
+print(df_config)
+
+
+df["Expense description"] = ""
+df["Expense category"] = ""
+df["Expense other category"] = ""
+df["Expense nature"] = ""
+
+print(df)
+
+df_results = pandas.DataFrame(columns=["Expense regex", "Expense amount",
+                                       "Expense category", "Expense category amount",
+                                       "Expense other category", "Expense other category amount",
+                                       "Expense nature", "Expense nature amount"])
+
+print(df_results)
+
+
+for idx1, config in df_config.iterrows():
+    expense_in_Ft = 0
+    regex_string = get_regex_str(idx1, df_config)
+
+    expense_description = df_config.at[idx1, "Expense description"]
+    expense_category = df_config.at[idx1, "Expense category"]
+    expense_other_category = df_config.at[idx1, "Expense other category"]
+    expense_nature = df_config.at[idx1, "Expense nature"]
+
+    for idx2, row2 in df.iterrows():
+
+        if re.search(regex_string, df.at[idx2, "Details"]):
+            print("Regex found" + regex_string)
+            df.at[idx2, "Expense description"] = expense_description
+            df.at[idx2, "Expense category"] = expense_category
+            df.at[idx2, "Expense other category"] = expense_other_category
+            df.at[idx2, "Expense nature"] = expense_nature
+            expense_in_Ft += df.at[idx2, "Transaction amount"]
+
+    df_results = df_results.append({"Expense regex": regex_string, "Expense amount": expense_in_Ft}, ignore_index=True)
+
+
+df_results = get_and_write_category_amount("Expense category", "Expense category amount", config_path, df_results, df)
+df_results = get_and_write_category_amount("Expense other category", "Expense other category amount", config_path, df_results, df)
+df_results = get_and_write_category_amount("Expense nature", "Expense nature amount", config_path, df_results, df)
+
+df.to_excel(file_path)
+
+df_results.to_excel(output_folder_path + "Combined_output_results.xlsx")
+
+pie_chart_list = [["Expense category", "Expense category amount", "K2", 'column'],
+                  ["Expense other category", "Expense other category amount", "K17", 'pie'],
+                  ["Expense nature", "Expense nature amount", "K32", 'pie']]
+
+cell_list = ["K2", "K17", "K32"]
+
+write_chart(os.path.join(output_folder_path, "Combined_output_results.xlsx"), pie_chart_list)
--- excel_third_project.py
+++ excel_third_project.py
@@ -4,36 +4,22 @@ import re
 import os
 from datetime import datetime
 import pandas
+from collections import namedtuple
+import numpy as np
+
+source_folder_path = "source_data\\"
+output_folder_path = "output\\"
+
+combined_file_name = "Combined_output.xlsx"
 
 
-source_data_folder_path = "source_data\\Peti\\"
 
-needed_columns_peti = ["Transaction date", "Transaction amount", "Details"]
+File_format = namedtuple('File_format', ['file_name', 'columns', 'header_index', 'source_folder', 'columns_to_merge'])
 
-file_list = os.listdir(source_data_folder_path)
-print(file_list)
-
-def delete_row_with_merged_ranges(sheet, idx):
-    begin_col, end_col = [], []
-    # Get column information of merged cell of row to delete
-    for n in sheet.merged_cells:
-        if (n.min_row == idx and n.max_row == idx):
-            begin_col.append(n.min_col)
-            end_col.append(n.max_col)
-    # Cancel cell merge of deleted row
-    for n in range(0, len(begin_col), 1):
-        sheet.unmerge_cells(
-            start_row=idx,
-            start_column=begin_col[n],
-            end_row=idx,
-            end_column=end_col[n]
-        )
-    sheet.delete_rows(idx)
-    for mcr in sheet.merged_cells:
-        if idx < mcr.min_row:
-            mcr.shift(row_shift=-1)
-        elif idx <= mcr.max_row:
-            mcr.shrink(bottom=1)
+peti = File_format("Peti_output.xlsx", ["Transaction date", "Transaction amount", "Merged_columns"], 3, source_folder_path + "Peti\\",
+                   ["Partner name/Secondary account identifier type", "Transaction type", "Details"])
+vanda = File_format("Vanda_output.xlsx", ["Tranzakció időpontja", "Összeg", "Merged_columns"], 13, source_folder_path + "Vanda\\",
+                    ["Forgalom típusa", "Ellenoldali név", "Közlemény"])
 
 
 def get_column_by_name(column_name, max_num_of_lines_from_1, worksheet):
@@ -53,18 +39,23 @@ def get_column_by_name(column_name, max_num_of_lines_from_1, worksheet):
     else:
         raise Exception("Can not find the given column!")
 
-def delete_row_before_column_name(column_name, column, worksheet):
+
+def get_column_num_by_name(column_name, max_num_of_lines_from_1, worksheet):
     done_flag = False
-    for idx, cell in enumerate(column, start=1):
-        if cell.value == column_name:
-            column_name_row_num = idx
-            done_flag = True
+    for idx in range(1, max_num_of_lines_from_1):
+        row = worksheet[idx]
+        for cell in row:
+            if cell.value == column_name:
+                column = cell.column
+                done_flag = True
+                break
+        if done_flag:
+            break
+
     if done_flag:
-        for idx2 in range(1, column_name_row_num+1):
-            print(idx2)
-            delete_row_with_merged_ranges(worksheet, idx2)
+        return column
     else:
-        raise Exception("Can not find the given column name!")
+        raise Exception("Can not find the given column!")
 
 
 def print_column(column):
@@ -72,52 +63,172 @@ def print_column(column):
         print(cell.value)
 
 
-def get_the_newest_file():
-    for idx, file in enumerate(file_list, start=1):
-        wb_newest = openpyxl.load_workbook(filename=file)
-        ws_newest = wb_newest.active
+def create_one_file_form_files(file_format):
+    output_file_path = output_folder_path + file_format.file_name
 
-        for idx2 in range(1, 10):
-            row_newest = ws_newest[idx2]
-            for cell_newest in row_newest:
-                pass
+    # Discover files in folder
+    file_list = os.listdir(file_format.source_folder)
+    files_path_list = []
+    for idx, file in enumerate(file_list):
+        files_path_list.append(file_format.source_folder + file)
+
+    # Combine all files in the folder in one dataframe
+    file_path = files_path_list[0]
+    print(file_path)
+    df = pandas.read_excel(file_path, header=file_format.header_index)
+    for idx in range(1, len(files_path_list)):
+        print(files_path_list[idx])
+        df2 = pandas.read_excel(files_path_list[idx], header=file_format.header_index)
+        frames = [df, df2]
+        df = pandas.concat(frames, ignore_index=True)
+
+    # Merge the useful columns into one
+    df[file_format.columns[2]] = " "
+    for col_name in file_format.columns_to_merge:
+        df[file_format.columns[2]] += " " + df[col_name].fillna(" ")
+
+    # Keep only the given columns
+    df = df[file_format.columns]
+
+    # Remove duplicated rows
+    df.drop_duplicates(subset=file_format.columns, keep="first", inplace=True)
+
+    # Rename all columns to unify them
+    df.rename(
+        columns={df.columns[0]: "Transaction date", df.columns[1]: "Transaction amount", df.columns[2]: "Details"},
+        inplace=True)
+
+    # Save dataframe into excel file
+    df.to_excel(output_file_path)
+
+    # Unify time format
+    unify_time_format(output_file_path)
+
+    # Reorder columns by date
+    order_rows_by_column(output_file_path, "Transaction date")
+
+    return output_file_path
 
 
-        "([0-9]{2})-([0-9]{2})-([0-9]{4})"
+def order_rows_by_column(file_path, column):
+    df = pandas.read_excel(file_path, index_col=0)
+    pandas.to_datetime(df[column])
+    df = df.sort_values(by=column, ascending=False)
+    df = df.reset_index(drop=True)
 
-        wb_newest.close()
+    df.to_excel(file_path)
 
 
-var1 = datetime.now()
-var2 = datetime(1993,1,23)
+def unify_time_format(file_path):
+    wb = openpyxl.load_workbook(filename=file_path)
+    ws = wb.active
+    column_details = get_column_by_name("Details", 10, ws)
+    column_details_num = get_column_num_by_name("Details", 10, ws)
+    column_time_num = get_column_num_by_name("Transaction date", 10, ws)
 
-if(var1<var2):
-    print(str(var1) + " is newer")
-else:
-    print(str(var2) + " is newer")
+    for idx in range(2, len(column_details) + 1):
+        result = re.search(".*(2[0-9])([0-1][0-9])([0-3][0-9])([0-2][0-9]):([0-6][0-9]).*",
+                           str(ws.cell(row=idx, column=column_details_num).value))
+        result2 = re.search(".*([0-9]{4}).([0-1][0-9]).([0-3][0-9]) ([0-2][0-9]):([0-6][0-9]):([0-6][0-9]).*",
+                           str(ws.cell(row=idx, column=column_details_num).value))
+        result3 = re.search(".*([0-1][0-9])-([0-3][0-9])-([0-9]{4}).*",
+                            str(ws.cell(row=idx, column=column_time_num).value))
+        result4 = re.search(".*([0-9]{4}).([0-1][0-9]).([0-3][0-9]). ([0-2][0-9]):([0-6][0-9]):([0-6][0-9]).*",
+                            str(ws.cell(row=idx, column=column_time_num).value))
 
-file_path = source_data_folder_path + file_list[0]
-print(file_path)
+        if result:
+            date = datetime(int(result.group(1)) + 2000, int(result.group(2)), int(result.group(3)),
+                            int(result.group(4)), int(result.group(5)))
+            # print(date)
+        elif result2:
+            date = datetime(int(result2.group(1)), int(result2.group(2)), int(result2.group(3)), int(result2.group(4)),
+                            int(result2.group(5)), int(result2.group(6)))
+            # print(date)
+        elif result3:
+            date = datetime(int(result3.group(3)), int(result3.group(1)), int(result3.group(2)))
+            # print(date)
+        elif result4:
+            date = datetime(int(result4.group(1)), int(result4.group(2)), int(result4.group(3)), int(result4.group(4)),
+                            int(result4.group(5)), int(result4.group(6)))
+            # print(date)
+        else:
+            print(str(ws.cell(row=idx, column=column_details_num).value))
+            print(str(ws.cell(row=idx, column=column_time_num).value))
+            print(str(ws.cell(row=idx, column=column_time_num).value))
+            raise Exception("Can not detect time!")
 
-#wb = openpyxl.load_workbook(filename=source_data_folder_path + file_list[0])
-#ws = wb.active
+        ws.cell(row=idx, column=column_time_num, value=str(date))
 
-#col_name = needed_columns_peti[0]
-#print_column(get_column_by_name(col_name, 5, ws))
+    wb.save(file_path)
 
-df = pandas.read_excel(file_path, header=3)
 
-for idx in range(1, len(file_list)):
-    print(source_data_folder_path + file_list[idx])
-    df2 = pandas.read_excel(source_data_folder_path + file_list[idx], header=3)
+def combine_two_files(file1, file2):
+    output_file_path = output_folder_path + combined_file_name
 
-    frames = [df, df2]
+    df1 = pandas.read_excel(file1, index_col=0)
+    df2 = pandas.read_excel(file2, index_col=0)
+
+    frames = [df1, df2]
     df = pandas.concat(frames, ignore_index=True)
 
-df = df[["Transaction date", "Transaction amount", "Details"]]
+    df.to_excel(output_file_path)
 
-df.drop_duplicates(subset=["Transaction date", "Transaction amount", "Details"], keep="first", inplace=True)
+    order_rows_by_column(output_file_path, "Transaction date")
 
-df.to_excel("output.xlsx")
+    return output_file_path
 
-#wb.save("test2.xlsx")
+
+def separate_file_by_date(file_path):
+    month_list = get_date_list(file_path, False)
+    year_list = get_date_list(file_path, True)
+
+    df = pandas.read_excel(file_path, index_col=0)
+    for date in month_list:
+        df_filter = df["Transaction date"].str.contains(".*" + date + ".*")
+        df2 = df[df_filter]
+        df2 = df2.reset_index(drop=True)
+        df2.to_excel(output_folder_path + date + "_output.xlsx")
+
+    for date in year_list:
+        df_filter = df["Transaction date"].str.contains(".*" + date + ".*")
+        df2 = df[df_filter]
+        df2 = df2.reset_index(drop=True)
+        df2.to_excel(output_folder_path + date + "_output.xlsx")
+
+
+def get_date_list(file_path, only_year):
+    wb = openpyxl.load_workbook(filename=file_path)
+    ws = wb.active
+    column_time = get_column_by_name("Transaction date", 10, ws)
+    column_time_num = get_column_num_by_name("Transaction date", 10, ws)
+
+    date_list = []
+
+    for idx in range(2, len(column_time) + 1):
+
+        result = re.search("([0-9]{4})-([0-1][0-9])", str(ws.cell(row=idx, column=column_time_num).value))
+        if result:
+            if only_year:
+                date = str(result.group(1))
+            else:
+                date = str(result.group(0))
+            if not date_list.count(date):
+                date_list.append(date)
+    return date_list
+
+
+def add_more_columns(folder_path):
+    # Discover files in folder
+    file_list = os.listdir(folder_path)
+    files_path_list = []
+    for idx, file in enumerate(file_list):
+        files_path_list.append(folder_path + file)
+
+
+output_file_path_peti = create_one_file_form_files(peti)
+output_file_path_vanda = create_one_file_form_files(vanda)
+
+output_combined_file_path = combine_two_files(output_file_path_peti, output_file_path_vanda)
+
+separate_file_by_date(output_combined_file_path)
+add_more_columns(output_folder_path)
